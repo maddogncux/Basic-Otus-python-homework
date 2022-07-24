79d45b34f46a01a5e2ca0284108b88efac69cbcf
maddogncux <madddogncux@gmail.com>
2022-07-24 14:59:27 +0300

    changed prime> is_prime + body changed Filter_numbers as mark in task - функция, которая на вход принимает список из целых чисел, и возвращает только чётные/нечётные/простые числа (выбор производится передачей дополнительного аргумента).     - Например: `filter_numbers([1, 2, 3], ODD)` вернёт `[1, 3]`, а `filter_numbers([2, 1, 3, 5, 4], EVEN)` вернёт `[2, 4]`     - **необходимо** использовать созданные константы `ODD`/`EVEN`/`PRIME` для проверок на тип фильтра       (то есть делать не `if filter_type == 'odd'`, а `if filter_type == ODD` и т.д.)
    mby i dont understend task!??????

--- homework_01/main.py
+++ homework_01/main.py
@@ -20,36 +20,31 @@ ODD = "odd"
 EVEN = "even"
 PRIME = "prime"
 
-#def primes_method3(n):
- #   n = numbers_list
- #   out = list()
- #  for num in range(1, n+1):
- #     if all(num % i != 0 for i in range(2, int(num**.5 ) + 1)):
- #            out.append(num)
- #   return out
 
-def prime(number_list):
-    prime_list = []
-    for i in number_list:
-        if i == 0 or i == 1:
-            continue
-        else:
-            for j in range(2, int(i / 2) + 1):
-                if i % j == 0:
-                    break
-            else:
-                prime_list.append(i)
-    return prime_list
 
-    #primes = []
-    #for number in number_list:
-    #    if all(number % i != 0 for i in range(2, int(number ** .5) + 1)):
-    #        primes.append(number)
+def is_prime(number):
+    if number > 1:  #if i == 0 or i == 1: continue
+        for i in range(2,number):
+            if number % i == 0:
+                return False
+    else:
+        return False
+    return True
 
-    #for number in number_list:
-    #   if all(number%i!=0 for i in range(2,int(math.sqrt(number))+1)):
-    #       primes.append(number)
-    #return primes
+
+
+    # prime_list = []
+    # for i in number_list:
+    #     if i == 0 or i == 1:
+    #         continue
+    #     else:
+    #         for j in range(2, int(i / 2) + 1):
+    #             if i % j == 0:
+    #                 break
+    #         else:
+    #             prime_list.append(i)
+    # return prime_list
+
 
 def filter_numbers(number_list, filter_type):
     """
@@ -62,12 +57,14 @@ def filter_numbers(number_list, filter_type):
     >>> filter_numbers([2, 3, 4, 5], EVEN)
     <<< [2, 4]
     """
-    if "odd" in filter_type:
+    if filter_type == ODD:
         return list(filter(lambda x: x % 2 != 0, number_list))
-    if "even" in filter_type:
+    # if "even" in filter_type:
+    if filter_type == EVEN:
         return list(filter(lambda x: x % 2 == 0, number_list))
-    if "prime" in filter_type:
-        return prime(number_list)
+    if filter_type == PRIME:
+        #return list(map(is_prime, number_list))
+        return list(filter(is_prime, number_list))
 
 
 
@@ -90,3 +87,40 @@ def filter_numbers(number_list, filter_type):
 
     #   return [number for number in number_list if 0 not in [number % i for i in range(2, int(number/2)+1)]] #1 error problem with 0,1 mby add
     # is_prime = lambda n: all( n%i != 0 for i in range(2, int(n**.5)+1) )
+
+    # def primes_method3(n):
+    #   n = numbers_list
+    #   out = list()
+    #  for num in range(1, n+1):
+    #     if all(num % i != 0 for i in range(2, int(num**.5 ) + 1)):
+    #            out.append(num)
+    #   return out
+    # def prime(number):
+
+    #     for i in range(2, number):
+    #         if number_list % i == 0:
+    #             return False
+    #
+    #     return True
+    #
+    # prime_list=list(map(prime, number_list))
+
+    # if number == 0 or number == 1:
+    #     return False
+    # for i in range(2, int(number // 2) + 1):
+    #     if number % i == 0:
+    #         return False
+    #     else:
+    #         return True
+
+    # return list(map(int(prime,number_list)))
+
+    # primes = []
+    # for number in number_list:
+    #    if all(number % i != 0 for i in range(2, int(number ** .5) + 1)):
+    #        primes.append(number)
+
+    # for number in number_list:
+    #   if all(number%i!=0 for i in range(2,int(math.sqrt(number))+1)):
+    #       primes.append(number)
+    # return primes
