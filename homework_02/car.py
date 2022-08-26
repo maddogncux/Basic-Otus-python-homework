f48e9aee52f25f4702ccf08478d8df32395d9b69
maddogncux <madddogncux@gmail.com>
2022-08-26 14:03:06 +0300

    Done Homework_3 all tests Passed container done web up done (not finished) comment done

--- homework_02/car.py
+++ homework_02/car.py
@@ -5,7 +5,7 @@ from homework_02.engine import Engine
 """
 
 
-class Car(Vehicle, Engine):
+class Car(Vehicle):
 
     def set_engine(self, Engine):
-        self.engine = Engine
+        self.engine = engine
