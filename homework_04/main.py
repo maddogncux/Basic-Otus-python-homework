a50b3a199d2fb1d8f376090fdc8773e2de80c76b
maddogncux <madddogncux@gmail.com>
2022-09-17 08:46:02 +0300

    fix homework 1 fix req for home work 5

--- homework_04/main.py
+++ homework_04/main.py
@@ -14,31 +14,7 @@
 - закрытие соединения с БД
 """
 
-# async def created_db_tables():
-#     async with engine.begin() as conn:
-#         await conn.run_sync(Base.metadata.drop_all)
-#         await conn.run_sync(Base.metadata.create_all)
-#
-#
-# async def save_user_in_db(u_data):
-#     async with Session() as session:
-#         async with session.begin():
-#             for user in u_data:
-#                 name = user['name']
-#                 email = user['email']
-#                 user = User(name=name, email=email)
-#                 session.add(user)  # добавляем данные юзера в сессию
-#                 # session.commit() делается авт
-#
-#             async def save_post_in_db(p_data):
-#                 async with Session() as session:
-#                     async with session.begin():
-#                         for post in p_data:
-#                             title = post['title']
-#                             description = post['body']
-#                             user_id = post['userId']
-#                             post = Post(title=title, description=description, user_id=user_id)
-#                             session.add(post)
+
 import asyncio
 
 from jsonplaceholder_requests import USERS_DATA_URL, POSTS_DATA_URL, fetch_json
@@ -69,3 +45,31 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+
+# async def created_db_tables():
+#     async with engine.begin() as conn:
+#         await conn.run_sync(Base.metadata.drop_all)
+#         await conn.run_sync(Base.metadata.create_all)
+#
+#
+# async def save_user_in_db(u_data):
+#     async with Session() as session:
+#         async with session.begin():
+#             for user in u_data:
+#                 name = user['name']
+#                 email = user['email']
+#                 user = User(name=name, email=email)
+#                 session.add(user)  # добавляем данные юзера в сессию
+#                 # session.commit() делается авт
+#
+#             async def save_post_in_db(p_data):
+#                 async with Session() as session:
+#                     async with session.begin():
+#                         for post in p_data:
+#                             title = post['title']
+#                             description = post['body']
+#                             user_id = post['userId']
+#                             post = Post(title=title, description=description, user_id=user_id)
+#                             session.add(post)
